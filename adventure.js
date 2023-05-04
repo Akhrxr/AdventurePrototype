7067704a03094b1ed6f0fd35f6a09775a9987da1
Akhrxr <akhrorgotswag@gmail.com>
2023-05-03 21:25:16 -0700

    Bunch a shi

--- adventure.js
+++ adventure.js
@@ -53,6 +53,26 @@ class AdventureScene extends Phaser.Scene {
 
     }
 
+    startDrag(pointer,targets) {
+        this.input.off('pointerdown',this.startDrag, this);
+        this.dragObj=targets[0];
+        this.input.on('pointermove',this.doDrag, this);
+        this.input.on('pointerup',this.stopDrag, this);
+
+    }
+
+    doDrag(pointer) {
+        this.dragObj.x = pointer.x;
+        this.dragObj.y = pointer.y;
+    }
+
+    stopDrag(pointer){
+        this.input.on('pointerdown',this.startDrag, this);
+        this.input.off('pointermove',this.doDrag, this);
+        this.input.off('pointerup',this.stopDrag, this);
+
+    }
+
     showMessage(message) {
         this.messageBox.setText(message);
         this.tweens.add({
--- game.js
+++ game.js
@@ -1,31 +1,39 @@
-class Demo1 extends AdventureScene {
+class Garage extends AdventureScene {
     constructor() {
-        super("demo1", "First Room");
+        super("garage", "Grab your keys and your wheel, we bouta slide.");
     }
-
+    preload(){
+        this.load.image('r32key',"assets/r32key.png");
+        this.load.image('swheel',"assets/steeringwheel.png");
+        this.load.image('r32car',"assets/r32white.png");
+        this.load.image('backgrounds',"assets/garage.jpg");
+    }
     onEnter() {
+        let background = this.add.image(-475, this.w * 0,"backgrounds")
+            .setScale(2)
+            .setOrigin(0,0);
 
-        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => this.showMessage("Metal, bent."))
+        let wheel = this.add.sprite(this.w * 0.2, this.w * 0.1, "swheel")
+            .setScale(0.2)
+            .setInteractive({useHandCursor:true})
+            .on('pointerover', () => this.showMessage("Nice n grippy."))
             .on('pointerdown', () => {
-                this.showMessage("No touching!");
+                this.showMessage("You pick up the wheel.");
+                this.gainItem('wheel');
                 this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
+                    targets: wheel,
+                    y: `-=${2 * this.s}`,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => wheel.destroy()
                 });
             });
 
-        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
-            .setFontSize(this.s * 2)
-            .setInteractive()
+        let key = this.add.sprite(this.w * 0.7, this.w * 0.25, "r32key")
+            .setScale(0.05)
+            .setInteractive({useHandCursor:true})
             .on('pointerover', () => {
-                this.showMessage("It's a nice key.")
+                this.showMessage("Perfect for starting a car")
             })
             .on('pointerdown', () => {
                 this.showMessage("You pick up the key.");
@@ -39,56 +47,75 @@ class Demo1 extends AdventureScene {
                 });
             })
 
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
+        let r32 = this.add.sprite(this.w * 0.4, this.w * 0.4, "r32car")
+            .setInteractive({useHandCursor:true})
+            .setScale(1.25)
             .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
+                if (this.hasItem("key") & this.hasItem("wheel")) {
+                    this.showMessage("You've got the key and wheel for the car.");
+                } else if (this.hasItem("key") & !this.hasItem("wheel")) {
+                    this.showMessage("You've got the key, but no wheel");
+                }
+                else {
+                    this.showMessage("You can't enter, get the key.");
                 }
             })
             .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
+                if (this.hasItem("key") & this.hasItem("wheel")) {
+                    this.loseItem("wheel");
+                    this.showMessage("*you entered*");
+                    this.gotoScene('cabin');
+                    this.tweens.add({
+                        targets: r32,
+                        alpha: { from: 1, to: 0 },
+                        duration: 1500,
+                        onComplete: () => r32.destroy()
+                    });                
                 }
             })
 
     }
 }
 
-class Demo2 extends AdventureScene {
+class Cabin extends AdventureScene {
     constructor() {
-        super("demo2", "The second room has a long name (it truly does).");
+        super("cabin", "Time to turn on the car");
+    }
+    preload() {
+        this.load.image('r32cabin',"assets/r32cabin.jpg");
+        this.load.image('r32key',"assets/r32key.png");
+        this.load.audio('startup', 'assets/startupgtr.mp3');
     }
     onEnter() {
-        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
-            .setFontSize(this.s * 2)
-            .setInteractive()
+        let background = this.add.image(-270, this.w * 0,"r32cabin")
+            .setScale(2)
+            .setOrigin(0,0);
+        this.input.on('pointerdown',this.startDrag, this);
+        this.gainItem('key');
+        let box = this.add.rectangle(1000,520,75,75)
+            .setFillStyle(0xff0000, 1);
+
+        let key = this.add.sprite(1600, 780, "r32key")
+            .setScale(0.05)
+            .setInteractive({useHandCursor:true})
             .on('pointerover', () => {
-                this.showMessage("You've got no other choice, really.");
+                this.showMessage("Perfect for starting a car. \nYou should put it in the ignition")
+
             })
             .on('pointerdown', () => {
-                this.gotoScene('demo1');
-            });
-
-        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage('*giggles*');
-                this.tweens.add({
-                    targets: finish,
-                    x: this.s + (this.h - 2 * this.s) * Math.random(),
-                    y: this.s + (this.h - 2 * this.s) * Math.random(),
-                    ease: 'Sine.inOut',
-                    duration: 500
-                });
+                this.showMessage("Put it in the ignition");  
             })
-            .on('pointerdown', () => this.gotoScene('outro'));
+
+        this.physics.add.overlap(key, box, function () {
+            inZone = true;
+        });
+        var clutch = scene.input.keyboard.addKey('C');
+        this.startupsound = this.sound.add("startup");
+    }
+    update() {
+        if (inZone && this.input.activePointer.isDown && clutch.isDown) {
+            
+        }
     }
 }
 
@@ -96,12 +123,18 @@ class Intro extends Phaser.Scene {
     constructor() {
         super('intro')
     }
+    preload(){
+        this.load.image('r32int', 'assets/r32.jpg');
+    }
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
+        this.background = this.add.image(0,0, "r32int");
+        this.background.setOrigin(0,0);
+        this.background.setScale(3.2);
+        this.add.text(50,50, "Welcome back DK").setFontSize(50);
         this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+            this.time.delayedCall(1000, () => this.scene.start('garage'));
         });
     }
 }
@@ -111,7 +144,7 @@ class Outro extends Phaser.Scene {
         super('outro');
     }
     create() {
-        this.add.text(50, 50, "That's all!").setFontSize(50);
+        this.add.text(50, 50, "Goodbye DK, good work today").setFontSize(50);
         this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
         this.input.on('pointerdown', () => this.scene.start('intro'));
     }
@@ -125,7 +158,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Demo1, Demo2, Outro],
+    scene: [Cabin, Intro, Garage, Outro],
     title: "Adventure Game",
 });
 
